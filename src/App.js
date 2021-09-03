f79b119d2ff209cf61418a0564599b500da5c75b
Nithish Kumar <nithishpadma@gmail.com>
2021-09-03 17:52:08 +0530

    add state logic for modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from './components/Modal/Modal'
 import './App.scss'
 
 const App = () => {
+    const [isOpen,setIsOpen] = useState(false)
+
     return(
         <div>
-            App
+            <button onClick={() => setIsOpen(true)}>Open modal</button>
+            {isOpen && <Modal onClose={setIsOpen} />}
         </div>
     )
 }
