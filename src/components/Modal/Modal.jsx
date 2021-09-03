0ea7a04c8d9c1e02d1526bf73b5b090d80433647
Nithish Kumar <nithishpadma@gmail.com>
2021-09-03 17:49:33 +0530

    style modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import ReactDOM from 'react-dom'
 import './Modal.scss'
 
-const Modal = () => {
-    return(
-        <div>
-            Modal
+const Modal = ({ onClose }) => {
+     const [name,setName] = useState('')
+     const [error,setError] = useState(null)
+     const inputRef = useRef(null)
+
+     useEffect(() => {
+         if(name.length < 3) {
+             setError(true)
+         } else {
+             setError(false)
+         }
+     },[name])
+
+     useEffect(() => {
+        inputRef.current.focus()
+     },[])
+
+     const handleNameChange = (evt) => {
+         setName(evt.target.value)
+     }
+
+     const handleClose = () => {
+        if(!error) {
+            onClose(false)
+        }
+     }
+
+     return ReactDOM.createPortal(
+        <>
+        <div className='overlay' />
+        <div className='modal'>
+           <p className="modal__title">Smart Diner</p>
+           <div className="modal__input-box">
+           <input ref={inputRef} type='text' className='modal__input' value={name} onChange={handleNameChange} placeholder='Enter name' />
+           <span className="modal__input-focus"></span>
+           </div>
+           {error && <small className='modal__error'>Username must be minimum of 3 characters</small>}
+           <button onClick={handleClose} className="modal__cta">Close</button>
         </div>
+       </>,
+        document.querySelector('#modal')
     )
 }
 
